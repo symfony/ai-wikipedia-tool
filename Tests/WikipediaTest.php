535ac8cfc63dafb875a635f5f33b62d93b2a137f
Oskar Stark <oskarstark@googlemail.com>
2025-11-24 11:13:46 +0100

    [Agent] Extract tools to dedicated bridges

---
+++ Tests/WikipediaTest.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Agent\Bridge\Wikipedia\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\AI\Agent\Bridge\Wikipedia\Wikipedia;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\JsonMockResponse;
+
+final class WikipediaTest extends TestCase
+{
+    public function testSearchWithResults()
+    {
+        $result = JsonMockResponse::fromFile(__DIR__.'/Fixtures/search-result.json');
+        $httpClient = new MockHttpClient($result);
+
+        $wikipedia = new Wikipedia($httpClient);
+
+        $actual = $wikipedia->search('current secretary of the united nations');
+        $expected = <<<EOT
+            Articles with the following titles were found on Wikipedia:
+             - Under-Secretary-General of the United Nations
+             - United Nations secretary-general selection
+             - List of current permanent representatives to the United Nations
+             - United Nations
+             - United Nations Secretariat
+             - Flag of the United Nations
+             - List of current members of the United States House of Representatives
+             - Member states of the United Nations
+             - Official languages of the United Nations
+             - United States Secretary of State
+
+            Use the title of the article with tool "wikipedia_article" to load the content.
+            EOT;
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testSearchWithoutResults()
+    {
+        $result = JsonMockResponse::fromFile(__DIR__.'/Fixtures/search-empty.json');
+        $httpClient = new MockHttpClient($result);
+
+        $wikipedia = new Wikipedia($httpClient);
+
+        $actual = $wikipedia->search('weird questions without results');
+        $expected = 'No articles were found on Wikipedia.';
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testArticleWithResult()
+    {
+        $result = JsonMockResponse::fromFile(__DIR__.'/Fixtures/article.json');
+        $httpClient = new MockHttpClient($result);
+
+        $wikipedia = new Wikipedia($httpClient);
+
+        $actual = $wikipedia->article('Secretary-General of the United Nations');
+        $expected = <<<EOT
+            This is the content of article "Secretary-General of the United Nations":
+            The secretary-general of the United Nations (UNSG or UNSECGEN) is the chief administrative officer of the United Nations and head of the United Nations Secretariat, one of the six principal organs of the United Nations. And so on.
+            EOT;
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testArticleWithRedirect()
+    {
+        $result = JsonMockResponse::fromFile(__DIR__.'/Fixtures/article-redirect.json');
+        $httpClient = new MockHttpClient($result);
+
+        $wikipedia = new Wikipedia($httpClient);
+
+        $actual = $wikipedia->article('United Nations secretary-general');
+        $expected = <<<EOT
+            The article "United Nations secretary-general" redirects to article "Secretary-General of the United Nations".
+
+            This is the content of article "Secretary-General of the United Nations":
+            The secretary-general of the United Nations (UNSG or UNSECGEN) is the chief administrative officer of the United Nations and head of the United Nations Secretariat, one of the six principal organs of the United Nations. And so on.
+            EOT;
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testArticleMissing()
+    {
+        $result = JsonMockResponse::fromFile(__DIR__.'/Fixtures/article-missing.json');
+        $httpClient = new MockHttpClient($result);
+
+        $wikipedia = new Wikipedia($httpClient);
+
+        $actual = $wikipedia->article('Blah blah blah');
+        $expected = 'No article with title "Blah blah blah" was found on Wikipedia.';
+
+        $this->assertSame($expected, $actual);
+    }
+}
---
+++ Wikipedia.php
@@ -0,0 +1,113 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Agent\Bridge\Wikipedia;
+
+use Symfony\AI\Agent\Toolbox\Attribute\AsTool;
+use Symfony\AI\Agent\Toolbox\Source\HasSourcesInterface;
+use Symfony\AI\Agent\Toolbox\Source\HasSourcesTrait;
+use Symfony\AI\Agent\Toolbox\Source\Source;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+/**
+ * @author Christopher Hertel <mail@christopher-hertel.de>
+ */
+#[AsTool('wikipedia_search', description: 'Searches Wikipedia for a given query', method: 'search')]
+#[AsTool('wikipedia_article', description: 'Retrieves a Wikipedia article by its title', method: 'article')]
+final class Wikipedia implements HasSourcesInterface
+{
+    use HasSourcesTrait;
+
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        private string $locale = 'en',
+    ) {
+    }
+
+    /**
+     * @param string $query The query to search for on Wikipedia
+     */
+    public function search(string $query): string
+    {
+        $result = $this->execute([
+            'action' => 'query',
+            'format' => 'json',
+            'list' => 'search',
+            'srsearch' => $query,
+        ], $this->locale);
+
+        $titles = array_map(fn (array $item) => $item['title'], $result['query']['search']);
+
+        if ([] === $titles) {
+            return 'No articles were found on Wikipedia.';
+        }
+
+        $result = 'Articles with the following titles were found on Wikipedia:'.\PHP_EOL;
+        foreach ($titles as $title) {
+            $result .= ' - '.$title.\PHP_EOL;
+        }
+
+        return $result.\PHP_EOL.'Use the title of the article with tool "wikipedia_article" to load the content.';
+    }
+
+    /**
+     * @param string $title The title of the article to load from Wikipedia
+     */
+    public function article(string $title): string
+    {
+        $response = $this->execute([
+            'action' => 'query',
+            'format' => 'json',
+            'prop' => 'extracts|info|pageimages',
+            'titles' => $title,
+            'explaintext' => true,
+            'redirects' => true,
+        ], $this->locale);
+
+        $article = current($response['query']['pages']);
+
+        if (\array_key_exists('missing', $article)) {
+            return \sprintf('No article with title "%s" was found on Wikipedia.', $title);
+        }
+
+        $result = '';
+        if (\array_key_exists('redirects', $response['query'])) {
+            foreach ($response['query']['redirects'] as $redirect) {
+                $result .= \sprintf('The article "%s" redirects to article "%s".', $redirect['from'], $redirect['to']).\PHP_EOL;
+            }
+            $result .= \PHP_EOL;
+        }
+
+        $this->addSource(
+            new Source($article['title'], $this->getUrl($article['title']), $article['extract'])
+        );
+
+        return $result.'This is the content of article "'.$article['title'].'":'.\PHP_EOL.$article['extract'];
+    }
+
+    /**
+     * @param array<string, mixed> $query
+     *
+     * @return array<string, mixed>
+     */
+    private function execute(array $query, ?string $locale = null): array
+    {
+        $url = \sprintf('https://%s.wikipedia.org/w/api.php', $locale ?? $this->locale);
+        $response = $this->httpClient->request('GET', $url, ['query' => $query]);
+
+        return $response->toArray();
+    }
+
+    private function getUrl(string $title): string
+    {
+        return \sprintf('https://%s.wikipedia.org/wiki/%s', $this->locale, str_replace(' ', '_', $title));
+    }
+}
